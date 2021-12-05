2beb35207113a011407b56601b6f4a4e05704558
guilherme <guilherme@GuilhermePC.localdomain>
2021-12-05 19:16:25 -0300

    add first problem

---
+++ 00/0.c
@@ -0,0 +1,9 @@
+// 11921ECP001
+#include <stdio.h>		// Biblioteca para entrada e saida de dados
+
+int main(){
+  int a, b;			// Variaveis que armazenam os valores inteiros
+  scanf("%d %d", &a, &b);	// Leitura dos valores inteiros nas variaveis a e b
+  printf("X = %d\n", a + b);	// Printa o resultado da soma das dois valores lidos
+  return 0;			// finaliza a funcao principal
+}
