f4244bcbe25256b9f8e6ce5c738c6ff957e30358
GuilhermeGabriel <guilhermegabriel90@gmail.com>
2022-02-17 10:06:18 -0300

    4-100%

--- 04/04.c
+++ 04/04.c
@@ -1,40 +1,74 @@
+// GUILHERME GABRIEL FERREIRA SOUZA
+// 11921ECP001
+
+// Biblioteca para entrada e saida de dados
 #include <stdio.h>
+
+// Biblioteca para manipulacao de memoria
 #include <stdlib.h>
+
+// Biblioteca importada para usar funcoes de operacoes de strings
 #include <string.h>
+
+// Interface para manipular arvores
 #include "arvore.h"
 
 int main(){
+  // Criacao e leitura da quantidade de nos e de operacoes
   int n,m,a,b; scanf("%d %d", &n, &m);
+
+  // Aloca um array de char dinamicamente para armazenar a operacao
   char *comando=(char*)malloc(5*sizeof(char));
 
+  // Aloca uma array de No's dinamicamente com a quantidade
+  // de arvores que terao na floresta inicial
   No** arvores=(No**)malloc((n+1)*sizeof(No*));
-  for(int i=0;i<=n;i++)arvores[i]=criar_no(i,NULL,NULL);
+  for(int i=0;i<=n;i++)arvores[i]=criar_no(i);
 
+  // Enquanto houver comandos
   while(m--){
+    // Le o comando
     scanf("%s", comando);
 
+    // Caso o comando seja o de lca
     if(strcmp(comando,"lca")==0){
+      // Recebe os noh's que sao para encontrar o lca
       scanf("%d %d", &a, &b);
-      lca(arvores[a],arvores[b]);
+      // Printa o valor retornado pela funcao lca
+      printf("%d\n",lca(arvores[a],arvores[b]));
     }
 
+    // Caso o comando seja o de link
     if(strcmp(comando,"link")==0){
+      // Recebe os nos que sao para fazer o link
       scanf("%d %d", &a, &b);
+
+      // Se os noh's forem diferentes, chama a funcao
+      // que faz o link dos mesmos
       if(a!=b)link(arvores[a],arvores[b]);
     }
 
+    // Caso o comando seja o de cut
     if(strcmp(comando,"cut")==0){
+      // Recebe o noh que eh para fazer o cut
       scanf("%d", &a);
+
+      // Chama a funcao cut, passando o noh 
+      // que eh para fazer o cut
       cut(arvores[a]);
     }
   }
 
+  // Libera da memoria o array de No's alocado dinamicamente 
+  // com a quantidade de arvores que terao na floresta inicial
   for(int i=0;i<=n;i++){
     free(arvores[i]);
-    //destruir_arvore(&arvores[i]);
   }
-  free(comando);
   free(arvores);
 
+  // Libera o array de char que armazena cada comando
+  free(comando);
+
+  // Encerra o programa
   return 0;
 }
--- 04/arvore.h
+++ 04/arvore.h
@@ -1,33 +1,32 @@
 // GUILHERME GABRIEL FERREIRA SOUZA
 // 11921ECP001
 
-// Uso da diretiva ifndef para caso a constante ARVORE_BINARIA_H
+// Uso da diretiva ifndef para caso a constante ARVORE
 // esteja definida, nao compilar o bloco de codigo novamente e evitar problemas.
-#ifndef ARVORE_BINARIA_H
-#define ARVORE_BINARIA_H
+#ifndef ARVORE
+#define ARVORE
 
 // Criacao de um tipo struct no que contem
-// um dado e um ponteiro para o filho da esquerda e da direita.
+// um dado e um ponteiro para o seu pai
 typedef struct no {
   int dado;
-  struct no *esq, *dir, *pai;
+  struct no *pai;
 } No;
 
 // Definicao de uma funcao de cria um noh da arvore
-// recebendo o valor que o noh ira armazenar, 
-// seu noh da esquerda e seu noh da direita
-No* criar_no(int x, No *l, No *r);
+// recebendo o valor que o noh ira armazenar
+No* criar_no(int x);
 
-// Definicao de funcao recursiva que 
-// elimina a arvore da memoria
-void destruir_arvore(No **raiz);
-
-void imprimir_arvore(No *raiz);
-No* procurar_no(No *raiz, int x);
-int numero_nos(No *raiz);
-int altura(No *raiz);
+// Definicao de uma funcao link que recebe dois noh's: filho e pai
+// linka o filho com o seu pai
 void link(No* filho, No* pai);
+
+// Definicao de uma funcao cut que recebe um noh
+// e corta sua ligacao com o seu pai
 void cut(No* no);
-void lca(No* a, No* b);
+
+// Definicao de uma funcao lca que recebe dois noh's e 
+// retorna qual o seu menor ancestral comum entre eles
+int lca(No* a, No* b);
 
 #endif
